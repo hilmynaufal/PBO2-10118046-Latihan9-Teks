90df4f0d179920ddf130f946c1b62e6357d7cba7
ASUS <ASUS@DESKTOP-6TMOBF9>
2019-09-26 19:20:29 +0700

    commit

---
+++ src/pbo2/pert1/pkg10118046/latihan9/teks/PBO2PERT110118046Latihan9Teks.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pbo2.pert1.pkg10118046.latihan9.teks;
+
+/**
+ *
+ * @author
+ *  Nama              : M. Hilmy Naufal
+ *  Kelas             : IF2
+ *  NIM               : 10118046
+ *  Deskripsi Program : menampilkan shortcut line
+ */
+public class PBO2PERT110118046Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char t = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + "berisi " + c);
+        
+    }
+
+}
